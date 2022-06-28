ff485ae44360f327064050d9c1bdb97e402a632d
Perry J.R. Rijaard <93517178+Ch5pper@users.noreply.github.com>
2022-06-28 15:24:20 +0200

    Update ResetButton.py
    lol2

--- ResetButton.py
+++ ResetButton.py
@@ -1,9 +1,11 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
 
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+while True:
+BUTTON = 17
+GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-while True: # Run forever
-    if GPIO.input(17) == GPIO.HIGH:
-        print("Button was pushed!")
+if GPIO.input(BUTTON) == FALSE:
+    print("Hij doet het!")
+else:
+    print("Hij doet het niet!")
